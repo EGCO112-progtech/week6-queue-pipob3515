e6ec8803ab7d52a038ef4515805614830236b7a0
NATTHAPAT TECHAPORNHIRAN <natthapat.tec@student.mahidol.edu>
2025-02-12 09:02:40 +0000

    add file

--- main.c
+++ main.c
@@ -8,23 +8,26 @@
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
    NodePtr tailPtr=NULL;
-/* For struct Queue
   Queue  q;
    q. headPtr=NULL;
    q.tailPtr=NULL;
    q.size=0;
-*/
+
+
    int i,x;
    
 
  for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
+            if(q.size!=0 ) {
+               x=dequeue_struct(&q);
+               printf("dequeing %d\n",x);
+            }
+            else printf("Empty Queue\n");
         }
         else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
+       enqueue_struct(&q, atoi(argv[i]));
+          
         }
  }
   return 0;
